6db045fac1ebeb056f287392ed4a81049b65f45a
Aya <shimizu@bu.edu>
2018-10-21 18:07:30 -0400

    finished mini_project2

---
+++ neuralnet.py
@@ -0,0 +1,126 @@
+# Mini-project 2
+# Aya Shimizu
+# Code taken through Tensorflow Tutorial; Modified for Two-class Classification
+import tensorflow as tf 
+from tensorflow import keras
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+import random
+
+''' First part reads in dataset '''
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(len(train_labels))
+dress_index = np.where(train_labels == 3)
+print(dress_index[0])
+shirt_index = np.where(train_labels == 6)
+indices = np.append(dress_index[0],shirt_index[0])
+train_images = train_images[indices]
+train_labels = train_labels[indices]
+
+testdress_index = np.where(test_labels == 3)
+testshirt_index = np.where(test_labels == 6)
+testindices = np.append(testdress_index[0], testshirt_index[0])
+test_images = test_images[testindices]
+test_labels = test_labels[testindices]
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+
+''' Second part to shape the dataset images and train the neural net'''
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+model.fit(train_images, train_labels, epochs=5)
+
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+
+predictions[0]
+np.argmax(predictions[0])
+test_labels[0]
+
+
+''' Third part to plot the results '''
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+  
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1]) 
+  predicted_label = np.argmax(predictions_array)
+ 
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+
+# Plot the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+num_rows = 5
+num_cols = 6
+num_images = num_rows*num_cols
+randinx = len(predictions)
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  x = random.randint(0, randinx)
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(x, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(x, predictions, test_labels)
+
+plt.show()
+# Grab an image from the test dataset
+img = test_images[0]
+print(img.shape)
+
+# Add the image to a batch where it's the only member.
+img = (np.expand_dims(img,0))
+
+print(img.shape)
+
+predictions_single = model.predict(img)
+
+print(predictions_single)
+
+plot_value_array(0, predictions_single, test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
